973ae0618679559c81df16931c59b010b8d6df94
Anri Shvangiradze <158323942+Shvangiradze-Anri@users.noreply.github.com>
2025-04-03 14:15:15 +0400

    Update logout.ts

--- frontend/src/hook/logout.ts
+++ frontend/src/hook/logout.ts
@@ -1,12 +1,13 @@
 import { axiosI } from "../../api/axios";
 
 export const handleLogout = async () => {
+  const navigate = useNavigate();
   try {
     await axiosI.post("/logout");
 
     localStorage.clear();
 
-    window.location.replace("https://web3-assignment.netlify.app/login");
+    navigate("/login")
   } catch (error) {
     // console.error("Logout failed:", error);
   }
